2f27d198546dc388dfe51e1a46de0f03f342feec
Matas Svarlys <matas.svarlys3@gmail.com>
2023-08-16 10:19:49 +0300

    first commit

---
+++ src/main/java/Accenture/HigherOrLower/HigherOrLowerApplication.java
@@ -0,0 +1,13 @@
+package Accenture.HigherOrLower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigherOrLowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HigherOrLowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Accenture/HigherOrLower/HigherOrLowerApplicationTests.java
@@ -0,0 +1,13 @@
+package Accenture.HigherOrLower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HigherOrLowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
