2aabd189fffe5c2d0b9ad928597ed74d7099434e
Augustinas <bukasaugustinas@gmail.com>
2023-08-17 13:54:59 +0300

    Added CORS for front-end

--- src/main/java/Accenture/HigherOrLower/Application.java
+++ src/main/java/Accenture/HigherOrLower/Application.java
@@ -2,12 +2,27 @@ package Accenture.HigherOrLower;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:5500") // Add your frontend domain
+						.allowedMethods("GET", "POST"); // Add allowed HTTP methods
+			}
+		};
 	}
 }
--- src/main/java/Accenture/HigherOrLower/CelebrityDatabase.java
+++ src/main/java/Accenture/HigherOrLower/CelebrityDatabase.java
@@ -1,66 +1,22 @@
 package Accenture.HigherOrLower;
 
-import Accenture.HigherOrLower.Celebrity;
-import Accenture.HigherOrLower.repository.CelebrityRepository;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import Accenture.HigherOrLower.repository.CelebrityRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class CelebrityDatabase {
 
     private final CelebrityRepository celebrityRepository;
 
-    @Autowired
     public CelebrityDatabase(CelebrityRepository celebrityRepository) {
         this.celebrityRepository = celebrityRepository;
 
-        // Load and insert data from Excel file
-        try (InputStream excelInputStream = getClass().getResourceAsStream("/Celebrities.xlsx")) {
-            insertCelebritiesFromExcel(excelInputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Transactional
-    public void insertCelebritiesFromExcel(InputStream excelInputStream) {
-        List<Celebrity> celebritiesFromExcel = parseExcel(excelInputStream);
-        celebrityRepository.saveAll(celebritiesFromExcel);
-    }
-
-    private List<Celebrity> parseExcel(InputStream excelInputStream) {
-        List<Celebrity> celebrities = new ArrayList<>();
-
-        try (Workbook workbook = WorkbookFactory.create(excelInputStream)) {
-            Sheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
-
-            for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
-                Row row = sheet.getRow(rowIndex);
-
-                String name = row.getCell(0).getStringCellValue();
-                String country = row.getCell(1).getStringCellValue();
-                int googleSearchCount = (int) row.getCell(2).getNumericCellValue();
-
-                Celebrity celebrity = new Celebrity(name, country, googleSearchCount);
-                celebrities.add(celebrity);
-            }
-        } catch (IOException | EncryptedDocumentException /*| InvalidFormatException*/ e) {
-            e.printStackTrace(); // Handle exceptions appropriately
-        }
-
-        return celebrities;
+        celebrityRepository.save(new Celebrity("Jonas Valanciunas", "Lithuania", 1000000));
+        celebrityRepository.save(new Celebrity("Kristaps Porzingis", "Latvia", 900000));
+        celebrityRepository.save(new Celebrity("Rolands Smits", "Latvia", 900000));
     }
 
     public List<Celebrity> getListOfCelebritiesByCountry(String countryCode) {
