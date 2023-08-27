cc9b4f27c0874dd6cabfe2641d22ce80561025dd
Matas Svarlys <matas.svarlys3@gmail.com>
2023-08-27 17:51:20 +0300

    couldnt get it working :(

--- src/main/java/Accenture/HigherOrLower/controller/PlayerController.java
+++ src/main/java/Accenture/HigherOrLower/controller/PlayerController.java
@@ -5,6 +5,9 @@ import Accenture.HigherOrLower.repository.PlayerRepository;
 import Accenture.HigherOrLower.service.GameService;
 import Accenture.HigherOrLower.service.impl.GameServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,17 +25,19 @@ public class PlayerController {
 
     private Player loggedInPlayer;
 
-    private boolean loggedIn;
+
     @GetMapping("/")
-    public String showTopScores(Model model) {
+    public String showTopScores(Model model, Authentication authentication) {
         List<Player> topScores = playerRepository.getTop5Players();
-        model.addAttribute("loggedIn", loggedIn);
         model.addAttribute("Score", topScores);
-        if (loggedInPlayer != null) {
+        //Make the authenticator go brr
+        if (authentication != null && authentication.isAuthenticated()) {
+            Player loggedInPlayer = (Player) authentication.getPrincipal();
+            model.addAttribute("loggedIn", true);
             model.addAttribute("Id", loggedInPlayer.getId());
-        }
-        else {
-            model.addAttribute("Id", (-1));
+        } else {
+            model.addAttribute("loggedIn", false);
+            model.addAttribute("Id", -1);
         }
         gameServiceImpl.createGame();
 
@@ -50,63 +55,36 @@ public class PlayerController {
 //        return "index";
 //    }
 
-    @GetMapping("/login")
-    public String loginValidation(Model model){
+//    @PostMapping("/signup")
+//    public String signUp(@RequestParam String name, @RequestParam String password, @RequestParam int age) {
+//        // Validate the input if needed
+//        if (name == null || name.isEmpty() || password == null || password.isEmpty() || age < 7 || age == 0) {
+//            return "signup";
+//        }
+//
+//        // Create a new user entity
+//        Player newUser = new Player();
+//        newUser.setName(name);
+//        newUser.setPassword(password);
+//        newUser.setAge(age);
+//
+//        // Save the user to the database
+//        playerRepository.save(newUser);
+//
+//        // Redirect to a success page or login page
+//        return "redirect:/login";
+//    }
 
-        return "login";
-    }
-    @GetMapping("/signup")
-    public String signupValidation(Model model){
 
-        return "signup";
-    }
-    @PostMapping("/signup")
-    public String signUp(@RequestParam String name, @RequestParam String password, @RequestParam int age) {
-        // Validate the input if needed
-        if (name == null || name.isEmpty() || password == null || password.isEmpty() || age < 7 || age == 0) {
-            return "signup";
-        }
 
-        // Create a new user entity
-        Player newUser = new Player();
-        newUser.setName(name);
-        newUser.setPassword(password);
-        newUser.setAge(age);
-
-        // Save the user to the database
-        playerRepository.save(newUser);
-
-        // Redirect to a success page or login page
-        return "redirect:/login";
-    }
-
-    @PostMapping("/login")
-    public String logIn(@RequestParam String name, @RequestParam String password) {
-        Player player = playerRepository.findByName(name);
-
-        if (player == null) {
-//            model.addAttribute("error", "Player not found");
-            return "login"; // Return to the login page with an error message
-        }
-
-        if (!player.getPassword().equals(password)) {
-            return "login"; // Return to the login page with an error message
-        }
-
-        loggedIn = true;
-        loggedInPlayer = player;
-        return "redirect:/";
-    }
-
-    @GetMapping("/logout")
-    public String logOut() {
-        // Reset the loggedIn state
-        loggedIn = false;
-        loggedInPlayer = null;
-
-        // Redirect to the login page or home page
-        return "redirect:/"; // or another appropriate URL
-    }
+//    @GetMapping("/logout")
+//    public String logOut() {
+//        // Reset the loggedIn state
+//        loggedInPlayer = null;
+//
+//        // Redirect to the login page or home page
+//        return "redirect:/"; // or another appropriate URL
+//    }
 
     @GetMapping("/end/{pid}")
     public String getEnd(Model model, @PathVariable(name = "pid") int pid) {
